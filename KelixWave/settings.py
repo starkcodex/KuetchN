eda734934e07b87c5196e1c07c8f4111bb15e50f
starkcodex <maneeshemn@gmail.com>
2024-06-03 19:58:26 +0530

    Changed Default database to PGadmin settings.

--- KelixWave/settings.py
+++ KelixWave/settings.py
@@ -65,11 +65,17 @@ WSGI_APPLICATION = 'KelixWave.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Kelix_db',
+        'USER': 'mcodex',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
+
 
 
 # Password validation
